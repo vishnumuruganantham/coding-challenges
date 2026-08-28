4f6d8eddc0d26beac9f6b232f9581f0e0990a381
Vishnu <vishnu.hihi@gmail.com>
2026-08-28 16:32:53 +0530

    first coding program

---
+++ medium/001_two_sum.py
@@ -0,0 +1,16 @@
+# Problem 5: Two Sum
+# Input: [2, 7, 11, 15], target = 9  |  Output: indices [0, 1] (2 + 7 = 9)
+# Very common interview problem. The dict approach is expected.
+
+
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        compliment = target - num
+        if compliment in seen:
+            return [seen[compliment], i]
+        seen[num] = i
+    return None
+
+
+print(two_sum([2, 7, 11, 15], 9))
