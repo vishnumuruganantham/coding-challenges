79827c4c19d36c9ef9be7b2519640b0bfb184fa7
Vishnu Muruganantham <vishnum30894@gmail.com>
2026-09-08 08:54:13 +0530

    prime number

---
+++ medium/004_prime_number.py
@@ -0,0 +1,15 @@
+"""Return whether a number is prime or not"""
+
+
+def is_prime(n):
+
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+print(is_prime(2))
