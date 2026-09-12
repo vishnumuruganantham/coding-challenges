b402fb6e0d74d033b3eae6c4718223e51a4c95ce
Vishnu Muruganantham <vishnum30894@gmail.com>
2026-09-12 13:48:29 +0530

    roman to integer

---
+++ difficult/005_roman_to_integer.py
@@ -0,0 +1,91 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+
+# Example 1:
+
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+# Example 2:
+
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+# Example 3:
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+# Constraints:
+
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        six = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+        symbol = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+
+        for key, value in six.items():
+            count = s.count(key)
+            if count > 0:
+                total = total + (value * count)
+                s = s.replace(key, "")
+        for char in s:
+            total = total + symbol[char]
+
+        return total
+
+    def romanToInt_2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        symbol = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+
+        for i, char in enumerate(s):
+            if i < len(s) - 1 and symbol[char] < symbol[s[i + 1]]:
+                total = total - symbol[char]
+            else:
+                total = total + symbol[char]
+
+        return total
+
+
+solution_instance = Solution()
+print(solution_instance.romanToInt("III"))  # Output: 3
+print(solution_instance.romanToInt("VIII"))  # Output: 8
+print(solution_instance.romanToInt("LVIII"))  # Output: 58
+print(solution_instance.romanToInt("MCMXCIV"))  # Output: 1994
+
+print(solution_instance.romanToInt_2("III"))  # Output: 3
+print(solution_instance.romanToInt_2("VIII"))  # Output: 8
+print(solution_instance.romanToInt_2("LVIII"))  # Output: 58
+print(solution_instance.romanToInt_2("MCMXCIV"))  # Output: 1994
