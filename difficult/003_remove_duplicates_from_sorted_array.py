ee491b8932d209c055cab6a8fd08f6e145ba8717
Vishnu Muruganantham <vishnum30894@gmail.com>
2026-09-12 09:09:34 +0530

    error fixing

--- difficult/003_remove_duplicates_from_sorted_array.py
+++ difficult/003_remove_duplicates_from_sorted_array.py
@@ -1,6 +1,6 @@
 """Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
-Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+ Consider the number of unique elements in nums to be k ,After removing duplicates, return the number of unique elements k
 
 The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
 
@@ -53,7 +53,7 @@ class Solution(object):
 
 
 solution_instance = Solution()
-nums = [1, 1, 2, 3, 4, 5]
-result = solution_instance.removeDuplicates(nums)
+input_nums = [1, 1, 2, 3, 4, 5]
+result = solution_instance.removeDuplicates(input_nums)
 
 print(result)
